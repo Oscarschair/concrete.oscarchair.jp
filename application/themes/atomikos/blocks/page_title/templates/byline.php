f6a27bdc7ff46fe98f7ae70484436231296b6f17
oscarchair <chair1homan@gmail.com>
2022-06-12 00:04:49 +0900

    1

--- application/themes/atomikos/blocks/page_title/templates/byline.php
+++ application/themes/atomikos/blocks/page_title/templates/byline.php
@@ -8,7 +8,7 @@ $site = app('site')->getSite();
 $config = $site->getConfigRepository();
 $publicProfilesEnabled = $config->get('user.profiles_enabled');
 
-//$response = $c->getAttribute('thumbnail');
+$response = $page->getAttribute('thumbnail');
 //   echo '<script>';
 //   echo 'console.log('. $response .')';
 //   echo '</script>';
