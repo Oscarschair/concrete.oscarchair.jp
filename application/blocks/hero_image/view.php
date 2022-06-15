25fb53f9785c19d748edd59077a6ab538126d1e1
BBF\chair.ho-man <chair@bb-f.jp>
2022-06-15 17:38:37 +0900

    switch to image 2

--- application/blocks/hero_image/view.php
+++ application/blocks/hero_image/view.php
@@ -57,7 +57,7 @@ if ($image2 === null) {
 </div>
 <div data-transparency="element" class="ccm-block-hero-image sp" <?php if ($height2) { ?>style="min-height: <?=$height2?>vh"<?php } ?>>
     <div class="ccm-block-hero-image-cover" <?php if ($height2) { ?>style="min-height: <?=$height2?>vh"<?php } ?>></div>
-    <div style="background-image: url(<?= h("\"{$image->getURL()}\"") ?>); <?php if ($height2) { ?>min-height: <?=$height2?>vh<?php } ?>" class="ccm-block-hero-image-image"></div>
+    <div style="background-image: url(<?= h("\"{$image2->getURL()}\"") ?>); <?php if ($height2) { ?>min-height: <?=$height2?>vh<?php } ?>" class="ccm-block-hero-image-image"></div>
     <div class="ccm-block-hero-image-text" <?php if ($height2) { ?>style="min-height: <?=$height2?>vh"<?php } ?>>
         <?php
         if ((string) $title !== '' ) {
