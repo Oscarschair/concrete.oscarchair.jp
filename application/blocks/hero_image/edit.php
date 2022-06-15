00edf5b4dd982e7024253e249bcb0ea7711fafc2
BBF\chair.ho-man <chair@bb-f.jp>
2022-06-15 12:49:35 +0900

    add image chooser to editor

--- application/blocks/hero_image/edit.php
+++ application/blocks/hero_image/edit.php
@@ -14,7 +14,19 @@ use Concrete\Core\Form\Service\DestinationPicker\DestinationPicker;
     <legend><?=t('Basics')?></legend>
     <div class="mb-3">
         <label class="form-label" for="image"><?=t('Image')?></label>
-        <?php echo $fileManager->image('image', 'image', t('Choose Image2'), $image ?? null); ?>
+        <?php echo $fileManager->image('image', 'image', t('Choose Image'), $image ?? null); ?>
+    </div>
+    <div class="mb-3">
+        <label class="form-label" for="image"><?=t('Height')?></label>
+        <input class="form-range" type="range" name="height" id="heroImageHeight" min="20" max="100" onchange="updateHeroImageHeight(this.value)" value="<?=$height?>">
+        <div class="alert alert-info">
+            <?=t('Current Value:')?> <code><span data-value="height"></span></code>
+        </div>
+    </div>
+
+    <div class="mb-3">
+        <label class="form-label" for="image"><?=t('Image for mobile')?></label>
+        <?php echo $fileManager->image('image', 'image', t('Choose Image'), $image ?? null); ?>
     </div>
     <div class="mb-3">
         <label class="form-label" for="image"><?=t('Height')?></label>
