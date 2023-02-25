dbb4f87f9fd49e7817afb9a36b9744f7b34b8c6c
oscarchair <chair1homan@gmail.com>
2023-02-25 23:38:36 +0900

    change edited

--- application/blocks/hero_split/edit.php
+++ application/blocks/hero_split/edit.php
@@ -30,7 +30,7 @@ use Concrete\Core\Form\Service\DestinationPicker\DestinationPicker;
     </div>
 </fieldset>
 <fieldset class="mb-3">
-    <legend><?=t('Text')?></legend>
+    <legend><?=t('Left info.')?></legend>
     <div class="mb-3">
         <label class="form-label" for="title"><?=t('Title')?></label>
         <input type="text" name="title" class="form-control" value="<?=$title ?? null?>">
@@ -41,10 +41,6 @@ use Concrete\Core\Form\Service\DestinationPicker\DestinationPicker;
         echo $editor->outputBlockEditModeEditor('body', isset($body) ? LinkAbstractor::translateFromEditMode($body) : null);
         ?>
     </div>
-</fieldset>
-
-<fieldset class="mb-3">
-    <legend><?=t('Button')?></legend>
     <div class="mb-3">
         <label class="form-label" for="buttonText"><?=t('Button Text')?></label>
         <input type="text" name="buttonText" class="form-control" value="<?=$buttonText ?? null ?>">
@@ -52,33 +48,37 @@ use Concrete\Core\Form\Service\DestinationPicker\DestinationPicker;
             <?=t('Leave blank to omit the button.')?>
         </div>
     </div>
-    <div class="form-group">
-        <?php echo $form->label("buttonSize", t("Button Size")); ?>
-        <?php echo $form->select("buttonSize", [
-                '' => t('Regular'),
-                'lg' => t('Large'),
-                'sm' => t('Small')
-            ], $buttonSize ?? null);
+    <div class="mb-3">
+        <?php echo $form->label('buttonLink', t('Button Link')) ?>
+        <?php echo $destinationPicker->generate(
+            'imageLink',
+            $imageLinkPickers,
+            $imageLinkHandle,
+            $imageLinkValue
+        )
         ?>
     </div>
-    <div class="form-group">
-        <?php echo $form->label("buttonStyle", t("Button Style")); ?>
-        <?php echo $form->select("buttonStyle", [
-            '' => t('Regular'),
-            'outline' => t('Outline'),
-        ], $buttonStyle ?? null);
+</fieldset>
+
+<fieldset class="mb-3">
+    <legend><?=t('Right info.')?></legend>
+    <div class="mb-3">
+        <label class="form-label" for="title2"><?=t('Title')?></label>
+        <input type="text" name="title2" class="form-control" value="<?=$title2 ?? null?>">
+    </div>
+    <div class="mb-3">
+        <label class="form-label" for="body2"><?=t('Body')?></label>
+        <?php
+        echo $editor->outputBlockEditModeEditor('body', isset($body2) ? LinkAbstractor::translateFromEditMode($body2) : null);
         ?>
     </div>
-    <?php if ($themeColorCollection) { ?>
-        <label class="form-label" for="buttonColor"><?=t('Button Color')?></label>
-        <div data-vue="hero-image">
-            <concrete-theme-color-input
-                :color-collection='<?=json_encode($themeColorCollection)?>'
-                <?php if ($buttonColor) { ?> color="<?=$buttonColor ?? null?>"<?php } ?>
-                input-name="buttonColor">
-            </concrete-theme-color-input>
+    <div class="mb-3">
+        <label class="form-label" for="buttonText"><?=t('Button Text')?></label>
+        <input type="text" name="buttonText2" class="form-control" value="<?=$buttonText2 ?? null ?>">
+        <div class="help-block">
+            <?=t('Leave blank to omit the button.')?>
         </div>
-    <?php } ?>
+    </div>
     <div class="mb-3">
         <?php echo $form->label('buttonLink', t('Button Link')) ?>
         <?php echo $destinationPicker->generate(
