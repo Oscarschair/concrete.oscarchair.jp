976b9c67f5ceb9df17ad6b7efe374a988c00ada8
oscarchair <chair1homan@gmail.com>
2023-02-26 14:31:23 +0900

    a

--- application/blocks/hero_split/view.js
+++ application/blocks/hero_split/view.js
@@ -3,18 +3,22 @@ const right = document.querySelector(".right");
 const split = document.querySelector(".split");
 
 left.addEventListener("mouseenter", () => {
+  left.classList.add("hover-left");
   right.classList.add("hover-left");
 });
 
 left.addEventListener("mouseleave", () => {
+  left.classList.remove("hover-left");
   right.classList.remove("hover-left");
 });
 
 right.addEventListener("mouseenter", () => {
+  left.classList.add("hover-right");
   right.classList.add("hover-right");
 });
 
 right.addEventListener("mouseleave", () => {
+  left.classList.remove("hover-right");
   right.classList.remove("hover-right");
 });
 
