404f4df84ccd3a519188944a187e08763c145f7d
oscarschair <chair1homan@gmail.com>
2023-02-27 15:57:00 +0900

    yes

--- application/blocks/hero_split/view.php
+++ application/blocks/hero_split/view.php
@@ -35,7 +35,7 @@ if ($image2 === null) {
 }
 ?>
 
-    <div class="ccm-block-hero-split" style="<?php if ($height) { ?>min-height: <?=$height?>vh;<?php }?>">
+    <div class="hero-split" style="<?php if ($height) { ?>min-height: <?=$height?>vh;<?php }?>">
       <div class="split left" style="background-image: url(<?= h("\"{$image->getURL()}\"") ?>); <?php if ($height) { ?>min-height: <?=$height?>vh<?php } ?>;background-repeat: no-repeat;background-size: cover;background-position: center;">
       <?php
         if ((string) $title !== '' ) {
