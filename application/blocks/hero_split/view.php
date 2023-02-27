f6c1c3e1848510dfd4aaecd0066cd9e0f95fb0b3
oscarschair <chair1homan@gmail.com>
2023-02-27 15:27:23 +0900

    try

--- application/blocks/hero_split/view.php
+++ application/blocks/hero_split/view.php
@@ -75,47 +75,4 @@ if ($image2 === null) {
          ?>
       </div>
     </div>
-    
-<script type="text/javascript">
-    $(function() {
-        var windowSize = $(window).width();
-
-        const left = document.querySelector(".left");
-        const right = document.querySelector(".right");
-        const split = document.querySelectorAll(".split");
-
-        if (windowSize > 799) {
-           left.addEventListener("mouseenter", () => {
-                for(let i in split){
-                    if (split.hasOwnProperty(i)) {
-                        split[i].classList.add("hover-left");
-                    }
-                }
-            });
-
-            left.addEventListener("mouseleave", () => {
-                for(let i in split){
-                    if (split.hasOwnProperty(i)) {
-                        split[i].classList.remove("hover-left");
-                    }
-                }
-            });
-
-            right.addEventListener("mouseenter", () => {
-                for(let i in split){
-                    if (split.hasOwnProperty(i)) {
-                        split[i].classList.add("hover-right");
-                    }
-                }
-            });
-
-            right.addEventListener("mouseleave", () => {
-                for(let i in split){
-                    if (split.hasOwnProperty(i)) {
-                        split[i].classList.remove("hover-right");
-                    }
-                }
-            });
-        }
-    })
-</script>
+ 
