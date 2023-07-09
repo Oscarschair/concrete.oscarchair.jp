77ac66cd52ce907883964209ed24ed63b114f61f
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-07-09 19:29:17 +0900

    bar

--- application/themes/atomikos/elements/footer_bottom.php
+++ application/themes/atomikos/elements/footer_bottom.php
@@ -13,7 +13,7 @@
 <script type="text/javascript">$(function() {
     $('#contents').scrollgress({//バーの高さの基準となるエリア指定
 		height: '5px',//バーの高さ
-		color: '#990000',//バーの色
+		color: var(--bs-primary),//バーの色
 	});	
 });
 </script>
