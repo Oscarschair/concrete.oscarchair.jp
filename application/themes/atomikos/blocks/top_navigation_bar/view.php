a1a8acea955f57cc0e536583bdfa22cbd5d83b57
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-07-09 17:48:19 +0900

    asd

--- application/themes/atomikos/blocks/top_navigation_bar/view.php
+++ application/themes/atomikos/blocks/top_navigation_bar/view.php
@@ -1,7 +1,7 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
 
 <div class="ccm-block-top-navigation-bar" <?php if ($includeTransparency) { ?>style="display: none" data-transparency="navbar"<?php } ?>>
-    <nav class="navbar navbar-expand-lg navbar-light <?php if ($includeStickyNav) { ?>fixed-top<?php } ?>">
+    <nav id="header" class="navbar navbar-expand-lg navbar-light <?php if ($includeStickyNav) { ?>fixed-top<?php } ?>">
         <div class="container-fluid">
             <a class="navbar-brand" href="<?=$home->getCollectionLink()?>">
                 <?php if ($logo && ($includeBrandLogo && $includeBrandText)) { ?>
