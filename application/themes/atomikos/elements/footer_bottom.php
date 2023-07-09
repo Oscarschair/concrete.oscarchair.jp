e2b49b1b89fe06ae7ff437c842620f2910ed2b3c
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-07-09 21:09:42 +0900

    defer

--- application/themes/atomikos/elements/footer_bottom.php
+++ application/themes/atomikos/elements/footer_bottom.php
@@ -3,8 +3,8 @@
 </div>
 
 <?php View::element('footer_required'); ?>
-<script type="text/javascript" src="<?=$view->getThemePath()?>/js/main.js"></script>
-<script type="text/javascript" src="<?=$view->getThemePath()?>/js/scrollgress.js?<?php echo time();?>"></script>
+<script type="text/javascript" src="<?=$view->getThemePath()?>/js/main.js" defer></script>
+<script type="text/javascript" src="<?=$view->getThemePath()?>/js/scrollgress.js?<?php echo time();?>" defer></script>
 <!--testing2-->
 <!--script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script-->
 <!--testing2-->
