83e37f3c53ec7db7755a15937513cd88a2ea5589
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-07-09 20:26:50 +0900

    mix js

--- application/themes/atomikos/elements/footer_bottom.php
+++ application/themes/atomikos/elements/footer_bottom.php
@@ -4,7 +4,6 @@
 
 <?php View::element('footer_required'); ?>
 <script type="text/javascript" src="<?=$view->getThemePath()?>/js/main.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/scrollgress/2.0.0/scrollgress.min.js"></script>
 <script type="text/javascript" src="<?=$view->getThemePath()?>/js/scrollgress.js"></script>
 <!--testing2-->
 <!--script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script-->
