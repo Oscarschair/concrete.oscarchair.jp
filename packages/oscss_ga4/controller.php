579384a753df0773c099518ddc431cda51c3ed1e
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-10-19 16:40:29 +0900

    s

--- packages/oscss_ga4/controller.php
+++ packages/oscss_ga4/controller.php
@@ -9,7 +9,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'oscss_ga4';
     protected $appVersionRequired = '9.0';
-    protected $pkgVersion = '1.0';
+    protected $pkgVersion = '0.1';
     
 
     public function getPackageName()
