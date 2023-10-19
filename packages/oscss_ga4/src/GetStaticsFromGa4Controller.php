efae0f91165c87c06a60cb5cfd7944b18896ecd8
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-10-19 18:33:23 +0900

    rertry

--- packages/oscss_ga4/src/GetStaticsFromGa4Controller.php
+++ packages/oscss_ga4/src/GetStaticsFromGa4Controller.php
@@ -1,5 +1,5 @@
 <?php
-namespace Concrete\Package\OscssGa4\src;
+namespace Concrete\Package\OscssGa4\Src;
 
 use Concrete\Package\OscssGa4\Src\Getting\Command\GetStaticsFromGa4Command;
 use Concrete\Core\Command\Task\Input\InputInterface;
@@ -11,23 +11,23 @@ use Concrete\Core\Command\Task\Runner\CommandTaskRunner;
 
 defined('C5_EXECUTE') or die("Access Denied.");
 
-class GetStaticsFromGa4Controller extends AbstractController
+class GetStaticsFromGa4Controller //extends AbstractController
 {
 
-    public function getName(): string
-    {
-        return t('Get Statics From Ga4');
-    }
+//     public function getName(): string
+//     {
+//         return t('Get Statics From Ga4');
+//     }
 
-    public function getDescription(): string
-    {
-        return t('Get Statics From Ga4 and write in database.');
-    }
+//     public function getDescription(): string
+//     {
+//         return t('Get Statics From Ga4 and write in database.');
+//     }
 
-    public function getTaskRunner(TaskInterface $task, InputInterface $input): TaskRunnerInterface
-    {
-//        $command = new GetStaticsFromGa4Command();
-//        return new CommandTaskRunner($task, $command, t('Success.'));
-    }
+//     public function getTaskRunner(TaskInterface $task, InputInterface $input): TaskRunnerInterface
+//     {
+// //        $command = new GetStaticsFromGa4Command();
+// //        return new CommandTaskRunner($task, $command, t('Success.'));
+//     }
 
 }
