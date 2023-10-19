b95af3dc9420611213996c735fffbd5cfd648423
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-10-19 16:49:45 +0900

    s

--- packages/oscss_ga4/src/Command/Task/Controller/GetStaticsFromGa4Controller.php
+++ packages/oscss_ga4/src/Command/Task/Controller/GetStaticsFromGa4Controller.php
@@ -2,7 +2,7 @@
 namespace Concrete\Package\OscssGa4\Src\Command\Task\Controller\GetStaticsFromGa4Controller;
 
 use Concrete\Core\Command\Task\Input\InputInterface;
-use Concrete\Core\Command\Task\Runner\CommandTaskRunner;
+// use Concrete\Core\Command\Task\Runner\CommandTaskRunner;
 use Concrete\Core\Command\Task\Runner\TaskRunnerInterface;
 use Concrete\Core\Command\Task\TaskInterface;
 use Concrete\Core\Command\Task\Controller\AbstractController;
