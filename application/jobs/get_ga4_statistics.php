31c51695da64c41e47aa3a6dbcc4bf65ed9496e0
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-10-19 13:14:48 +0900

    CronExpression.php

---
+++ application/jobs/get_ga4_statistics.php
@@ -0,0 +1,22 @@
+<?php
+namespace Application\Job;
+use \Concrete\Core\Job\Job as AbstractJob;
+
+class GetGa4Statistics extends AbstractJob
+{
+
+    public function getJobName()
+    {
+        return t("Get Ga4 Statistics.");
+    }
+
+    public function getJobDescription()
+    {
+        return t("Get detailed statistics from GA4 and summarizes.");
+    }
+
+    public function run()
+    {
+        
+    }
+}
