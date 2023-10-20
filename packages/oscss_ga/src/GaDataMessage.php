142adf0f07d324c818283504dca97e4226bc6eae
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-10-20 13:47:13 +0900

    s

--- packages/oscss_ga/src/GaDataMessage.php
+++ packages/oscss_ga/src/GaDataMessage.php
@@ -1,10 +1,11 @@
 <?php
 namespace Concrete\Package\OscssGa\Src;
-use Concrete\Core\Conversation\Message\Message;
+//use Concrete\Core\Conversation\Message\Message;??
+use Concrete\Core\Foundation\Command\Command;
 
 defined('C5_EXECUTE') or die("Access Denied.");
 
-class GaDataMessage extends Message
+class GaDataMessage extends Command
 {
 
 }
