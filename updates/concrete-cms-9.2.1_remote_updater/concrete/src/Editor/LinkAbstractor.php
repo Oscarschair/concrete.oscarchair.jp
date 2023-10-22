54bb69e7ff3405cd38fedacbb5070b5995249152
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-10-22 18:17:41 +0900

    abc

--- updates/concrete-cms-9.2.1_remote_updater/concrete/src/Editor/LinkAbstractor.php
+++ updates/concrete-cms-9.2.1_remote_updater/concrete/src/Editor/LinkAbstractor.php
@@ -79,7 +79,7 @@ class LinkAbstractor extends ConcreteObject
                 [
                     '{CCM:CID_\\1}',
                     '{CCM:FID_DL_\\1}',
-                    '{CCM:BASE_URL}',
+                    '{CCM:BASE_URL}a',
                 ],
                 $text
             );
@@ -106,7 +106,7 @@ class LinkAbstractor extends ConcreteObject
 
         $text = preg_replace(
             [
-                '/{CCM:BASE_URL}/i',
+                '/{CCM:BASE_URL}b/i',
             ],
             [
                 Application::getApplicationURL(),
@@ -270,7 +270,7 @@ class LinkAbstractor extends ConcreteObject
 
         $text = preg_replace(
             [
-                '/{CCM:BASE_URL}/i',
+                '/{CCM:BASE_URL}c/i',
             ],
             [
                 $appUrl,
