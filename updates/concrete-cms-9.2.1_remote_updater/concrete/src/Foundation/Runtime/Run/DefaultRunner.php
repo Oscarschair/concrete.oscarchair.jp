ccd7aa772ef897118a30432c4d89c746a7322714
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-10-23 19:39:20 +0900

    try

--- updates/concrete-cms-9.2.1_remote_updater/concrete/src/Foundation/Runtime/Run/DefaultRunner.php
+++ updates/concrete-cms-9.2.1_remote_updater/concrete/src/Foundation/Runtime/Run/DefaultRunner.php
@@ -144,7 +144,9 @@ class DefaultRunner implements RunInterface, ApplicationAwareInterface
     {
         if (!defined('BASE_URL')) {
             $resolver = $this->getUrlResolver();
-
+            echo '<script>';
+            echo 'console.log("pppppppppppppppppppppppppppppppppppppppppppppppppppp")';
+            echo '</script>';
             try {
                 $url = rtrim((string) $resolver->resolve([]), '/');
                 define('BASE_URL', $url);
