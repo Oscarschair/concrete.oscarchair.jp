fe414b80ef1f26d8ae79a6dbf2a24eb8bba1839a
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-11-26 13:20:52 +0900

    try

--- application/themes/atomikos/elements/footer_bottom.php
+++ application/themes/atomikos/elements/footer_bottom.php
@@ -5,9 +5,7 @@
 <?php View::element('footer_required'); ?>
 <script type="text/javascript" src="<?=$view->getThemePath()?>/js/main.js" defer></script>
 <script type="text/javascript" src="<?=$view->getThemePath()?>/js/scrollgress.js?<?php echo time();?>" defer></script>
-<!--testing2-->
-<!--script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script-->
-<!--testing2-->
+<script type="text/javascript" src="<?=$view->getThemePath()?>/js/auto_blog_nav.js?<?php echo time();?>" defer></script>
 
 </body>
 </html>
---
+++ application/themes/atomikos/js/auto_blog_nav.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+	var target_id = 'auto_blog_nav';
+	var target_headline = 'h2.heading';
+	var target_contents = document.getElementById( target_id );
+
+    if( target_contents != null ){
+        var matches = document.querySelectorAll( target_headline );
+        var ol = document.createElement('ol');
+        matches.forEach( function (value, i) {
+            if ( value.id === '' ) {   // if tag has no id, add id
+                value.id = "blogLink".i;
+            }
+            var li = document.createElement('li');
+            var a = document.createElement('a');
+            a.innerHTML = value.innerHTML;
+            a.href = '#' + value.id;
+            li.appendChild(a);
+            ol.appendChild(li);
+        });
+        target_contents.appendChild(ol);
+    }
+});
