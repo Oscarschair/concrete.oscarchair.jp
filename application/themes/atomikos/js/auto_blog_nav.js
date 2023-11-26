24fef726fd08d2218977967234dc0109ccb3a269
LAPTOP-REUII5G1\hirof <chair1homan@gmail.com>
2023-11-26 13:32:04 +0900

    s

--- application/themes/atomikos/js/auto_blog_nav.js
+++ application/themes/atomikos/js/auto_blog_nav.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
         var matches = document.querySelectorAll( target_headline );
         var ol = document.createElement('ol');
         matches.forEach( function (value, i) {
+            i++;
             if ( value.id === '' ) {   // if tag has no id, add id
                 value.id = 'blogLink' + i;
             }
